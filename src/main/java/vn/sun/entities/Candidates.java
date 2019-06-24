3f5b6c7c96b4dd8ec6c975c7eea7270f2848bee6
LedSinh <le.dinh.sinh@sun-asterisk.com>
2019-06-24 13:11:21 +0700

    Init candidate

---
+++ src/main/java/vn/sun/entities/Candidates.java
@@ -0,0 +1,143 @@
+package vn.sun.entities;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.hibernate.annotations.Type;
+
+@Entity
+@Table(name = "candidates")
+public class Candidates {
+
+	@Id
+	@Column(name="id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(name="name")
+	private String name;
+	
+	@Column(name = "dob")
+	@Temporal(TemporalType.DATE)
+	private Date dob;
+	
+	@Column(nullable = false, columnDefinition = "TINYINT", length = 4)
+	private boolean gender;
+	
+	@Lob
+    @Column(name = "photo", columnDefinition="BLOB")
+    private byte[] image;
+	
+	@Column(name="city")
+	private String city;
+	
+	@Column(name = "address")
+	@Type(type = "text")
+	private String address;
+	
+	@Column(name="role")
+	private String role;
+	
+	@Column(name = "objective")
+	@Type(type = "text")
+	private String objective;
+	
+	@OneToOne
+	@JoinColumn(name="userId")
+	private Users users;
+
+	public Candidates() {
+
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Date getDob() {
+		return dob;
+	}
+
+	public void setDob(Date dob) {
+		this.dob = dob;
+	}
+
+	public boolean isGender() {
+		return gender;
+	}
+
+	public void setGender(boolean gender) {
+		this.gender = gender;
+	}
+
+	public byte[] getImage() {
+		return image;
+	}
+
+	public void setImage(byte[] image) {
+		this.image = image;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	public String getObjective() {
+		return objective;
+	}
+
+	public void setObjective(String objective) {
+		this.objective = objective;
+	}
+
+	public Users getUser() {
+		return users;
+	}
+
+	public void setUser(Users user) {
+		this.users = user;
+	}
+}
